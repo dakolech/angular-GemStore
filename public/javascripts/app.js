fbb7f4689ca7e5819c72a4157dc6e91de8def7e6
Daniel <dakolech@gmail.com>
2014-06-19 16:43:33 +0200

    level 05

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,6 +1,6 @@
 (function(){
 
-	var app = angular.module('store', [ ]);
+	var app = angular.module('store', ['store-products']);
 
 	app.controller('StoreController', function(){
 		this.products = gems;
@@ -35,45 +35,7 @@
 		};
 	});
 	
-	app.directive('productTitle', function(){
-		return {
-			restrict: 'A', 
-			templateUrl: './templates/product-title.html'
-		};
-	});
-	
-	app.directive('productPanels', function(){
-		return {
-			restrict: 'A',
-			templateUrl: './templates/product-panels.html',
-			controller: function() {
-				this.tab = 1;
-		
-				this.selectTab = function(setTab) {
-					this.tab = setTab;
-				};
-		
-				this.isSelected = function(checkTab){
-					return this.tab === checkTab;
-				};
-			},
-			controllerAs: 'panels'
-		};
-	});
-	
-	app.directive("productGallery", function() {
-		return {
-			restrict: "A",
-			templateUrl: "./templates/product-gallery.html",
-			controller: function() {
-				this.current = 0;
-				this.setCurrent = function(imageNumber){
-					this.current = imageNumber || 0;
-			 };
-		  },
-		  controllerAs: "gallery"
-		};
-  });
+
 	
 	var gems = [
 		{
