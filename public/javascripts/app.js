cc2d44f3b2142e9e6bbe2dc6ec6b2a6cd73561d8
Daniel <dakolech@gmail.com>
2014-06-19 15:03:07 +0200

    Level 04

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -27,6 +27,32 @@
 		};
 	});
 	
+	app.directive('productTitle', function(){
+		return {
+			restrict: 'A', 
+			templateUrl: './templates/product-title.html'
+		};
+	});
+	
+	app.directive('productPanels', function(){
+		return {
+			restrict: 'E',
+			templateUrl: './templates/product-panels.html',
+			controller: function() {
+				this.tab = 1;
+		
+				this.selectTab = function(setTab) {
+					this.tab = setTab;
+				};
+		
+				this.isSelected = function(checkTab){
+					return this.tab === checkTab;
+				};
+			},
+			controllerAs: 'panels'
+		};
+	});
+	
 	var gems = [
 		{
 			name: "Dodecahedron",
