ff4b973d036e51464e2546f572fe040c6a711658
Daniel <dakolech@gmail.com>
2014-06-20 15:28:34 +0200

    starting

---
+++ app/models/product.js
@@ -0,0 +1,11 @@
+var mongoose = require('mongoose');
+
+module.exports = mongoose.model('Product', {
+	name : String,
+	price : Number,
+	added: { type: Date, default: Date.now },
+	description: String,
+	canPurchase: Boolean,
+	
+	
+});
